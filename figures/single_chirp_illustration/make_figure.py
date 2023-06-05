b6d91c2a35174b789e673274d8b2511090e43bb0
Simon Hulse <jesu2901@parsley.chem.ox.ac.uk>
2023-06-05 16:27:55 +0100

    Cleaning up BBQCHILI discussion.

--- figures/single_chirp_illustration/make_figure.py
+++ figures/single_chirp_illustration/make_figure.py
@@ -1,7 +1,7 @@
 # make_figure.py
 # Simon Hulse
 # simon.hulse@chem.ox.ac.uk
-# Last Edited: Thu 01 Jun 2023 23:24:26 BST
+# Last Edited: Mon 05 Jun 2023 14:11:25 BST
 
 import nmrespy as ne
 import matplotlib as mpl
@@ -29,13 +29,13 @@ fig, ax = plt.subplots(
     nrows=1,
     ncols=1,
     gridspec_kw={
-        "left": 0.01,
-        "right": 0.99,
-        "bottom": 0.01,
+        "left": 0.,
+        "right": 1.,
+        "bottom": 0.2,
         "top": 0.99,
         "hspace": 0.,
     },
-    figsize=(5, 2),
+    figsize=(5, 2.5),
 )
 
 for x in ("left", "right", "top", "bottom"):
@@ -60,7 +60,7 @@ expinfo = ne.ExpInfo(dim=1, sw=sw, offset=sw / 2., default_pts=pts)
 
 chirp(ax, 0., tau_p, 0.5, top - 0.5)
 
-for freq, start in zip(freqs, starts):
+for i, (freq, start) in enumerate(zip(freqs, starts)):
     base_params[0, 2] = freq + 0.9 * ((sw / 2) - freq)
     fid = 0.23 * expinfo.make_fid(base_params) + (0.25 + 0.25j)
     tp = np.linspace(start, start + 80., pts)
@@ -70,7 +70,52 @@ for freq, start in zip(freqs, starts):
     ax.plot(tp[:tp_acqu_idx + 1], fid[:tp_acqu_idx + 1].imag, alpha=0.4, ls=":", color=color)[0]
     ax.plot(tp[tp_acqu_idx:], fid[tp_acqu_idx:].real, color=color)
     ax.plot(tp[tp_acqu_idx:], fid[tp_acqu_idx:].imag, color=color, ls=":")
-    ax.plot([start, start], [0.02, top], color=color, lw=1.)
+    ax.plot([start, start], [0., top], color=color, lw=1.)
+
+    arrow_y = 0.04 + i * 0.07
+    ax.plot(
+        [start, start],
+        [arrow_y, 0.2],
+        color=color,
+        lw=1.,
+        transform=fig.transFigure,
+        clip_on=False,
+        solid_capstyle="round",
+    )
+
+
+    ax.add_patch(
+        mpl.patches.ConnectionPatch(
+            xyA=[start, arrow_y],
+            xyB=[tau_p + tau_del, arrow_y],
+            coordsA="figure fraction",
+            coordsB="figure fraction",
+            lw=1.,
+            arrowstyle="<->",
+            color=color,
+        )
+    )
+
+    midpoint = (start + tau_p + tau_del) / 2
+    bbox = {"facecolor": "w", "edgecolor": "none", "pad": 1.5}
+    if i == 0:
+        color_txt = "red"
+    elif i == 1:
+        color_txt = "blue"
+    elif i == 2:
+        color_txt = "yellow"
+
+    ax.text(
+        midpoint,
+        arrow_y,
+        f"$t_0\\left(f^{{(1)}}_{{\\text{{{color_txt}}}}}\\right)$",
+        bbox=bbox,
+        ha="center",
+        va="center",
+        color=color,
+        transform=fig.transFigure,
+        fontsize=7,
+    )
 
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
@@ -78,6 +123,7 @@ ax.set_ylim(0, 1)
 ax.plot([0., 1.], [0.5, 0.5], color="k", transform=ax.transAxes, clip_on=False)
 
 ax.plot(2 * [tau_p + tau_del], [0., top], color="k")
+ax.plot(2 * [tau_p + tau_del], [0.04, 0.2], color="k", transform=fig.transFigure, clip_on=False)
 
 arrow = mpl.patches.ConnectionPatch(
     xyA=[0., (1. + top) / 2.],
