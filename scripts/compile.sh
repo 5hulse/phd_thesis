c8a0b77b8f0a842f3b77e51620122c9c620144e0
Simon Hulse <simonhulse@protonmail.com>
2023-09-14 12:49:13 +0100

    Tweaks to PDF insert chapter.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,7 +1,5 @@
 ls text/generated/*.py | xargs -L1 python &
-cp ~/Documents/DPhil/papers/cupid/main.pdf cupid-draft.pdf
-pdftk cupid-draft.pdf cat 1 output cupid-draft-page1.pdf
-pdftk cupid-draft.pdf cat 2-end output cupid-draft-page2-onwards.pdf
+cp ~/Documents/DPhil/papers/cupid/main_angew.pdf cupid-draft.pdf
 xelatex --shell-escape thesis &&
 biber thesis &&
 makeindex thesis.nlo -s nomencl.ist -o thesis.nls &&
