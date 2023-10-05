a46bcaee5b7a7842d9e9e70b8ee5b2a7d27fdbf8
5hulse <simonhulse@protonmail.com>
2023-10-05 19:33:34 +0100

    FINISHED!

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,4 @@
 # ls text/generated/*.py | xargs -L1 python &
-cp ~/Documents/DPhil/papers/cupid/main_angew.pdf cupid-draft.pdf
 xelatex --shell-escape thesis &&
 biber thesis &&
 makeindex thesis.nlo -s nomencl.ist -o thesis.nls &&
